d8cfefb24135f37fda19ee59311543515689233c
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-05-14 15:46:46 +0200

    refactor: move AABB to Geometry module

---
+++ src/Composer/Geometry.elm
@@ -0,0 +1,18 @@
+module Composer.Geometry exposing (BoundingBox)
+
+{-| Some geometry basics.
+
+@docs BoundingBox
+
+-}
+
+
+{-| An axis-aligned bounding box (also known as AABB). A box aligned with
+coordinate axes and enclosing some object.
+-}
+type alias BoundingBox =
+    { xMax : Float
+    , xMin : Float
+    , yMax : Float
+    , yMin : Float
+    }
