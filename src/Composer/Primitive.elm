e8813f0df01a30edc06cdeceb7851a37245585af
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-07-19 13:32:01 +0200

    refactor: simpler primitives

--- src/Composer/Primitive.elm
+++ src/Composer/Primitive.elm
@@ -1,29 +1,17 @@
 module Composer.Primitive
     exposing
-        ( Children(Children)
-        , Opacity
-        , Primitive(Rectangle, Group)
-        , origin
-        , rect
-        , size
+        ( Primitive(Rectangle, Texture)
         )
 
-{-| A primitive set of geometry units. Composer reduces other complex graphic
-languages to these set of geometry primitives. Also, composer backends are feed
-with values of this type.
+{-| A primitive set of geometry atoms. Scenes are reduce to a flat list of
+primitives by composer. Then, some backends are feed with these primitives to
+create pdf, svg, webgl scenes, etc.
 
-@docs Primitive, Opacity, Children
-
-
-# Queries
-
-@docs origin, rect, size
+@docs Primitive
 
 -}
 
 import Color exposing (Color)
-import Composer.Geometry.Point exposing (Point)
-import Composer.Geometry.Rect exposing (Rect)
 import Composer.Geometry.Size exposing (Size)
 import Composer.Geometry.Transform exposing (Transform)
 
@@ -33,39 +21,11 @@ type alias Opacity =
     Float
 
 
+type alias Id =
+    String
+
+
 {-| -}
 type Primitive
-    = Rectangle Rect Transform Opacity Color
-    | Group Rect Transform Opacity Children
-
-
-{-| A list of primitives used to avoid type recursion in primitive definition.
--}
-type Children
-    = Children (List Primitive)
-
-
-{-| Returns the origin of the given primitive.
--}
-origin : Primitive -> Point
-origin =
-    rect >> .origin
-
-
-{-| Returns the bounding rectangle of the given primitive.
--}
-rect : Primitive -> Rect
-rect primitive =
-    case primitive of
-        Rectangle rect _ _ _ ->
-            rect
-
-        Group rect _ _ _ ->
-            rect
-
-
-{-| Returns the size of the given primitive.
--}
-size : Primitive -> Size
-size =
-    rect >> .size
+    = Rectangle Id Transform Size Color
+    | Texture Id Transform Opacity Size String
